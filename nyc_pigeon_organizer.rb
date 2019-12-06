91d1713a01b9d9fcad2427b68eb4fa760d58adc1
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-06 19:40:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  put_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        if !put_hash[name]
+          put_hash[name] = {}
+        end
+        if !put_hash[name][key]
+          put_hash[name][key] = []
+        end
+        put_hash[name][key] << new_value.to_s
+      end
+    end
+  end
+  put_hash
 end
