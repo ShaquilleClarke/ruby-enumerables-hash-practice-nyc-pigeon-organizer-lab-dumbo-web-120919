47f7ef96693c129a87c8548e9c9e683e6a36cfbf
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-06 19:37:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  put_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        if !another_hash[name]
+          another_hash[name] = {}
+        end
+        if !another_hash[name][key]
+          another_hash[name][key] = []
+        end
+        another_hash[name][key] << new_value.to_s
+      end
+    end
+  end
+  another_hash
 end
